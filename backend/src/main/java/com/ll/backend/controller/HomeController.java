718eaf693d1d0943fa7c93e7d3b27a274f65ec8b
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-01-13 10:41:47 +0900

    work

---
+++ backend/src/main/java/com/ll/backend/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.ll.backend.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author shjung
+ * @since 25. 1. 13.
+ */
+@Controller
+public class HomeController {
+}
